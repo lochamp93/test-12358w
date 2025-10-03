ed61d8a5574c6ab7bbdd226822a481cbb89abcc5
lochamp93 <verjatc@yahoo.com>
2025-10-04 00:12:46 +0200

    Update script.js

--- orbix-12/settings/script.js
+++ orbix-12/settings/script.js
@@ -1,15 +1,23 @@
 // /orbix-12/settings/script.js  (LOADER)
 const widgetContainer = document.getElementById('widgetContainer');
-
 // Chemin vers le core réutilisable (note le _common)
 const settingsPageURL = '../../-common/core/settings-core';
-
 const currentURL = window.location.href;
-let baseURL = currentURL;
-if (baseURL.endsWith('index.html')) baseURL = baseURL.replace('index.html', '');
 
+let settingsJSON;
+let baseURL = currentURL;
+
+if (baseURL.endsWith("index.html"))
+    baseURL = baseURL.replace("index.html", "");
 // Construit les paramètres que le core attend
-const settingsJSON = '?settingsJson=' + baseURL + 'settings.json';
-const widgetURL    = '&widgetURL='    + baseURL.replace('/settings', '');
+settingsJSON = "?settingsJson=" + baseURL + "settings.json";
+
+const lastSlashIndex = baseURL.lastIndexOf("/");
+let widgetURL = "&widgetURL=" + baseURL.replace("/settings", "");
+
+console.debug("Window Ref: " + window.location.href);
+console.debug("Base URL: " + baseURL);
+console.debug("Settings JSON: " + settingsJSON);
+console.debug("Widget URL: " + widgetURL);
 
 widgetContainer.src = settingsPageURL + settingsJSON + widgetURL;
